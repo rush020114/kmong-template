97cbab92db12712cbd5e8071a68a3cf1a365834f
rush020114 <rush020114@gmail.com>
2025-11-25 20:08:27 +0900

    백엔드 프로젝트 만들기

---
+++ backend/src/test/java/com/kmong/template/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmong.template.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
