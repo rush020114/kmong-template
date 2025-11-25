945a0959ddf8786f77af5b8dc4a6715073639054
rush020114 <rush020114@gmail.com>
2025-11-25 21:37:17 +0900

    백엔드 cors설정 및 테이블 schema 생성

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+-- items
+CREATE TABLE items (
+    id INT PRIMARY KEY,                             -- 상품 아이디
+    title VARCHAR(100) NOT NULL,                    -- 상품명
+    content VARCHAR(1000) NOT NULL,                 -- 상품 설명
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- 등록일
+);
+
+-- item_images
+CREATE TABLE item_images (
+    id SERIAL PRIMARY KEY,                             -- 상품 이미지 아이디
+    origin_img_name VARCHAR(100) NOT NULL,             -- 원본 파일명
+    attached_img_name VARCHAR(100) NOT NULL,           -- 첨부 파일명
+    item_id INT REFERENCES items(id) ON DELETE CASCADE
+);
